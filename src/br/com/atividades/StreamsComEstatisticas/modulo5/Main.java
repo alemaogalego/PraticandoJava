4b7f16742e630b5a0607173b8898a04b183f69d0
Lucas César Lorena <lucascesar.lorena@gmail.com>
2026-08-17 19:44:12 -0300

    Novos exercicios Realizados.

---
+++ src/br/com/atividades/StreamsComEstatisticas/modulo5/Main.java
@@ -0,0 +1,21 @@
+package br.com.atividades.StreamsComEstatisticas.modulo5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6);
+        // código do particionamento da lista
+
+        Map<Boolean, List<Integer>> trocando = numeros.stream()
+                .collect(Collectors.partitioningBy(n -> n % 2 == 0));
+        System.out.println(trocando.get(true));
+        System.out.println(trocando.get(false));
+
+
+
+    }
+}
